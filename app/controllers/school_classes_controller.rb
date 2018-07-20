8a9e71b62f19c37f31546ee63e4d278d33177b33
Suchitra Naik <susanaik@gmail.com>
2018-07-20 18:12:36 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,9 +1,11 @@
 class SchoolClassesController < ApplicationController
 
   def index
+    @school_classes = SchoolClass.all
   end
 
   def new
+    @school_class = SchoolClass.new
   end
 
   def create
