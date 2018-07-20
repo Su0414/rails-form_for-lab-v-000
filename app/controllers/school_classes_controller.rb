87353b24b7360e43a45cfa3fa6a1a24aa25fec98
Suchitra Naik <susanaik@gmail.com>
2018-07-20 18:17:11 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -9,6 +9,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @school_class = Student.new(student_params(:title,:room_number))
+    @school_class.save
+	  redirect_to school_class_path(@school_class)
   end
 
   def show
@@ -20,5 +23,9 @@ class SchoolClassesController < ApplicationController
   def update
   end
 
+  private
+  def student_params(*args)
+    params.require(:school_class).permit(*args)
+  end
 
 end
